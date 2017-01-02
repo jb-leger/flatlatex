7106c2246e7bdf8e4c4e993d66067061b882d07c
Jean-Benoist Leger <jb@leger.tf>
2017-01-02 17:18:05 +0100

    version++

--- flatlatex/__init__.py
+++ flatlatex/__init__.py
@@ -61,7 +61,7 @@ __title__ = 'flatlatex'
 __author__ = 'Jean-Benoist Leger'
 __licence__ = 'BSD-2'
 
-version_info = (0, 2)
+version_info = (0, 3)
 __version__ = '.'.join(map(str, version_info))
 
 from .conv import converter
