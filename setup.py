285fffaef9d79cb6629993bc02842700cde8b165
Pierre-Elliott Bécue <becue@crans.org>
2018-03-18 13:10:15 +0100

    Add a flatlatex console_script.

--- setup.py
+++ setup.py
@@ -115,5 +115,8 @@ setup(
     # "scripts" keyword. Entry points provide cross-platform support and allow
     # pip to create the appropriate form of executable for the target platform.
     entry_points={
+        'console_scripts': [
+            "flatlatex=flatlatex.bin.flatlatex:main",
+        ],
     },
 )
