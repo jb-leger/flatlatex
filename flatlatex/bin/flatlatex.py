ed8d64fd9595c0a84277b89e77533f8180ac03d5
Pierre-Elliott Bécue <becue@crans.org>
2018-03-18 13:20:07 +0100

    Docstring for bin/flatlatex.py

--- flatlatex/bin/flatlatex.py
+++ flatlatex/bin/flatlatex.py
@@ -24,7 +24,8 @@
 # ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 # POSSIBILITY OF SUCH DAMAGE.
 """
-
+This is a rough binary command that casts a converter and calls
+convert on the user's input.
 """
 
 import argparse
